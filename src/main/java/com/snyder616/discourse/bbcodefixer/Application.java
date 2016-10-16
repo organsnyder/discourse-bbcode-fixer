38f80456043378073a4c30443c6d288dc830d089
Chris Snyder <chris@snyder616.com>
2016-10-16 16:29:55 -0400

    basic skeleton

---
+++ src/main/java/com/snyder616/discourse/bbcodefixer/Application.java
@@ -0,0 +1,9 @@
+package com.snyder616.discourse.bbcodefixer;
+
+import org.springframework.boot.SpringApplication;
+
+public class Application {
+  public static void main(String[] args) {
+    SpringApplication.run(DiscourseBbCodeFixer.class, args);
+  }
+}
---
+++ src/main/java/com/snyder616/discourse/bbcodefixer/DiscourseBbCodeFixer.java
@@ -0,0 +1,10 @@
+package com.snyder616.discourse.bbcodefixer;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+public class DiscourseBbCodeFixer {
+}
